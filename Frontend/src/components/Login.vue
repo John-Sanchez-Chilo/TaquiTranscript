70b5ac17d8336ad247f4ac1d15734122f0428b7e
John-Sanchez-Chilo <82964968+John-Sanchez-Chilo@users.noreply.github.com>
2022-10-05 11:13:50 -0500

    Update Login.vue

--- Frontend/src/components/Login.vue
+++ Frontend/src/components/Login.vue
@@ -1,112 +1,215 @@
 <template>
-  <v-app light>
-    <v-main class="">
+  <v-app>
+    <v-main class="light-blue darken-4">
     <v-content>
-      <section>
-        <v-parallax src="https://i.blogs.es/8ad7c5/notebook_work_girl_computer_woman_operator_design_business-1407325/1366_2000.jpg" height="700">
-          <v-layout column align-center justify-center class="white--text">
-            <h1 class="white--text mb-2 display-1 text-xs-center" style="font-weight: 900; text-shadow: 3px 2px #000000">La herramienta de trabajo para taquigrafos judiciales</h1>
-            <div class="white--text subheading mb-3 text-xs-center" style="font-weight: 900; text-shadow: 2px 2px #000000">Deja que Taquitranscript se encargue de todo y relajate</div>
-            <v-btn class="blue lighten-2 mt-5" dark large link @click="$router.push({ path: '/login' })">
-              Empieza ahora
-            </v-btn>
-          </v-layout>
-        </v-parallax>
-      </section>
 
-      <section>
-        <v-layout
-          column
-          wrap
-          class="my-5"
-          align-center
-        >
-          <v-flex xs12 sm4 class="my-3">
-            <div class="text-xs-center">
-              <h2 class="headline">La mejor manera de almacenar tus transcripciones</h2>
-              <span class="subheading">
-                No mas restricciones, no mas limites
-              </span>
-            </div>
-          </v-flex>
-          <v-flex xs12>
-            <v-container grid-list-xl>
-              <v-layout row wrap align-center>
-                <v-flex xs12 md4>
-                  <v-card class="elevation-0 transparent">
-                    <v-card-text class="text-xs-center">
-                      <v-icon x-large class="blue--text text--lighten-2">public</v-icon>
-                    </v-card-text>
-                    <v-card-title primary-title class="layout justify-center">
-                      <div class="headline text-xs-center">Entorno amigable</div>
-                    </v-card-title>
-                    <v-card-text>
-                      Nuestro software esta destinado a los taquigrafos judiciales, y esta pensado y creado con una interfaz sencilla y amigable facilitando su uso. De esta forma, los nuevos usuarios pueden aprender rapidamente su fucionamiento
-                    </v-card-text>
-                  </v-card>
-                </v-flex>
-                <v-flex xs12 md4>
-                  <v-card class="elevation-0 transparent">
-                    <v-card-text class="text-xs-center">
-                      <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
-                    </v-card-text>
-                    <v-card-title primary-title class="layout justify-center">
-                      <div class="headline">Transcripciones precisas</div>
-                    </v-card-title>
-                    <v-card-text>
-                      Nuestro software hace uso del servicio de Amazon (AWS) el cual se encarga de hacer las transcripciones mas precisas a  partir de los audios que recibe. De esta manera no tendras que pasar largas horas escuchando y escribiendo tus audios
-                    </v-card-text>
-                  </v-card>
-                </v-flex>
-                <v-flex xs12 md4>
-                  <v-card class="elevation-0 transparent">
-                    <v-card-text class="text-xs-center">
-                      <v-icon x-large class="blue--text text--lighten-2">share</v-icon>
-                    </v-card-text>
-                    <v-card-title primary-title class="layout justify-center">
-                      <div class="headline text-xs-center">Almacenamiento eficaz</div>
-                    </v-card-title>
-                    <v-card-text>
-                      Gracias a nuestra interfaz amigable, se puede acceder a los diferentes transcripciones almacenadas, ingresando a estas a traves de filtros de acceso rapido y divisiones a travès de casos, sesiones, participantes y declaraciones. 
-                    </v-card-text>
-                  </v-card>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-flex>
-        </v-layout>
-      </section>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="8">
+
+            <v-card class="elevation-12" fab color="#ccffff" outline height="520">
+              <v-window v-model="step" >
+                <v-window-item :value="1">
+                  <v-row>
+                    <v-col cols="12" md="8">
+                      <v-card-text class="mt-16">
+                        <h1
+                          class="text-center display-5 teal--blue text--blue" 
+                        >Iniciar Sesion en Seco System</h1>
+                        <div class="text-center mt-4">
+                          <v-btn class="mx-2" fab color="#0000ff" outlined>
+                            <v-icon>fab fa-facebook-f</v-icon>
+                          </v-btn>
+
+                          <v-btn class="mx-2" fab color="#ff0000" outlined>
+                            <v-icon>fab fa-google-plus-g</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="#3399ff" outlined>
+                            <v-icon>fab fa-linkedin-in</v-icon>
+                          </v-btn>
+                        </div>
+                        <h4 class="text-center mt-4">Ingresa tu correo y contraseña</h4>
+                        <v-form>
+                          <v-text-field
+                      
+                          label="Correo"
+                          prepend-icon="email"
+                          required
+                          />
+                          <v-text-field
+
+                            id="password"
+                            label="Contraseña"
+                            prepend-icon="lock"
+                            type="password"
+                          />
+                        </v-form>
+                      </v-card-text>
+                      <div class="text-center mt-3">
+                        <v-btn
+                          color="blue darken-4"
+                          v-bind="attrs"
+                          v-on="on"
+                          dark
+                          rounded
+                          link @click="$router.push({ path: '/admin' })"
+                        >
+                          INGRESAR
+                        </v-btn>
+                      </div>
+                    </v-col>
+                    <v-col cols="12" sm="4">
+                      <v-sheet color="blue darken-4" height="520" outlined>
+                      <v-card-text class="white--text mt-12">
+                        <h1 class="text-center display-1">¡Hola Amigo!</h1>
+                        <h5
+                          class="text-center"
+                        >Bienvenido a la semana de la computación, ingresa tus datos para resgistrarte</h5>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn rounded color="white" @click="step++">CREAR CUENTA</v-btn>
+                      </div>
+                    </v-sheet>
+                    </v-col>
+
+                  </v-row>
+                </v-window-item>
+                <v-window-item :value="2">
+                  <v-row class="fill-height">
+                    <v-col cols="12" md="4" class="blue darken-4">
+                      <v-sheet color="blue darken-4" height="520" outlined>
+                      <v-card-text class="white--text mt-12">
+                        <h1 class="text-center display-1">¡Hola! de nuevo</h1>
+                        <h5
+                          class="text-center"
+                        >Para ingresa al apartado principal, ingresar tus datos</h5>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn rounded  color="white" @click="step--">INGRESAR</v-btn>
+                      </div>
+                      </v-sheet>
+                    </v-col>
+
+                    <v-col cols="12" md="8">
+                      <v-card-text class="mt-8">
+                        <h1 class="text-center display-2 teal--blue text--accent-3">Crear Cuenta</h1>
+                        <div class="text-center mt-4">
+                          <v-btn class="mx-2" fab color="#0000ff" outlined>
+                            <v-icon>fab fa-facebook-f</v-icon>
+                          </v-btn>
+
+                          <v-btn class="mx-2" fab color="#ff0000" outlined>
+                            <v-icon>fab fa-google-plus-g</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="#3399ff" outlined>
+                            <v-icon>fab fa-linkedin-in</v-icon>
+                          </v-btn>
+                        </div>
+                        <h4 class="text-center mt-4">Ingresa tu correo y contraseña</h4>
+                        <v-form>
+                          <v-text-field
+                          v-model='newTask.nombre'
+                          :rules="nameRules"
+                          label="Participante"
+                          prepend-icon="person"
+                          required
+                          />
+                          <v-text-field
+                            v-model='newTask.email'
+                            :rules="nameRules"
+                            label="Correo"
+                            prepend-icon="email"
+                            type="text"
+                            color="#ff0000"
+                          />
+                          <v-text-field
+                            v-model='newTask.contrasenia'
+                            :rules="nameRules"
+                            id="password"
+                            label="Contraseña"
+                            prepend-icon="lock"
+                            type="password"
+                          />
+                        </v-form>
+                      </v-card-text>
+                      <div class="text-center mt-0">
+                        <v-btn
+                          color="blue darken-4"
+                          v-bind="attrs"
+                          v-on="on"
+                          dark
+                          rounded
+                          link @click="addTask"
+                        >
+                          CREAR CUENTA
+                        </v-btn>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+              </v-window>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
   </v-main>
-</v-app>
+  </v-app>
 </template>
 
-
-
-  
-  
-  <script>
+<script>
   import axios from 'axios'
-    export default {
-      data () {
-        return {
-          tasks: [],
-          postURL: 'https://backendsecosystem.herokuapp.com',
-          config_request: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-          },
-        }
+  
+  export default{    
+      data(){ 
+          return {
+              step: 1,
+              tasks: [],
+              newTask: {},
+              postURL: 'https://backendsecosystem.herokuapp.com',
+              config_request: {
+                  'Content-Type': 'application/json',
+                  'Access-Control-Allow-Origin': '*'
+              }       
+          }
+      },
+      props: {
+      source: String
       },
       methods:{
-        setmeet(){
-          window.location.href = "https://meet.google.com/ydt-sxvb-uwg";
-        }
+          addTask(){ 
+              axios.post(this.postURL + '/usuario/add_usuario', this.newTask, this.config_request)
+                  .then(res => {                                         
+                      this.tasks.push(res.data);
+                      console.log(res.data)        ;
+                  })
+                  .catch((error) => {
+                      console.log(error)
+                  });
+  
+              this.newTask = {};
+          },
+  
+          deleteTask(task){                      
+              axios.post(this.postURL + '/usuario/delete_usuario', {ID_Concurso: task.id_conc}, this.config_request)
+                  .then(() => {                      
+                      this.tasks.splice(this.tasks.indexOf(task), 1);                    
+                  })
+                  .catch((error) => {
+                      console.log(error)
+                  });  
+          },
+          reset(){                      
+              this.newTask = {};
+          }
       },
+  
       created(){ 
-          axios.post(this.postURL + '/actividad/get_actividades')
+          axios.post(this.postURL + '/usuario/get_usuarios')
               .then(res => { this.tasks = res.data; })
               .catch((error) => { console.log(error) })
-      },
-    }
+      }
+  
+  }
+  
   </script>
